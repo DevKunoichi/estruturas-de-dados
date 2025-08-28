cb9bc1999a006e5c4a2215a5d58710ca0d332295
ANDRESA NASCIMENTO DE PAULA <1090482421041@fatec-srv.local>
2025-08-28 09:47:30 -0300

    Primeiro Commit

---
+++ Métodos/Exercicio1.js
@@ -0,0 +1,29 @@
+//1° Método de Seleção Direta
+//Vetor com os números que queremos ordenar
+let vetorNumeros = [25, 57, 48, 37, 12, 92, 86, 33]
+
+// Método de Seleção Direta aplicado direto no vetor
+for (let i = 0; i < vetorNumeros.length - 1; i++) {
+    let indiceMenor = i
+
+    // Encontra o menor elemento do restante do vetor
+    for (let j = i + 1; j < vetorNumeros.length; j++) {
+        if (vetorNumeros[j] < vetorNumeros[indiceMenor]) {
+            indiceMenor = j
+        }
+    }
+
+    // Troca o menor elemento encontrado com o elemento da posição atual
+    if (indiceMenor !== i) {
+        let auxiliar = vetorNumeros[i]
+        vetorNumeros[i] = vetorNumeros[indiceMenor]
+        vetorNumeros[indiceMenor] = auxiliar
+    }
+}
+
+// Exibindo o vetor ordenado
+console.log("Vetor Ordenado:", vetorNumeros)
+
+//*************************************************************************************************
+
+
---
+++ Métodos/Exercicio2.js
@@ -0,0 +1,19 @@
+//2° Método Bubblesort
+//Vetor com os números que queremos ordenar
+let vetorNumeros = [25, 57, 48, 37, 12, 92, 86, 33]
+
+//Algoritmo Bubblesort aplicado direto no vetor
+for (let i = 0; i < vetorNumeros.length -1; i++) {
+    for (let j = 0; j < vetorNumeros.length -1 -i; j++) {
+        if (vetorNumeros [j] > vetorNumeros [j+1]) {
+            //Troca de posição entre os dois elementos
+            temporario = vetorNumeros [j]
+            vetorNumeros [j] = vetorNumeros [j+1]
+            vetorNumeros [j+1] = temporario
+        }
+    }
+}
+
+// Exibindo o vetor ordenado
+console.log("Vetor Ordenado:", vetorNumeros)
+
